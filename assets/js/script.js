3c7c50ac6f81876155f116722b81ddcdbb09ef15
Ovieh <ovieh.mosley@gmail.com>
2017-10-02 17:37:51 -0400

    Added bootstrap theme, favicon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,70 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAW0HeKoWv6GOoLdN1cLefoazTY7nTyOCE",
-    authDomain: "zepher-express.firebaseapp.com",
-    databaseURL: "https://zepher-express.firebaseio.com",
-    projectId: "zepher-express",
-    storageBucket: "zepher-express.appspot.com",
-    messagingSenderId: "1044678323580"
-  };
-  firebase.initializeApp(config);
+ (function () {
 
-  let database = firebase.database();
+   // Initialize Firebase
+   var config = {
+     apiKey: "AIzaSyAW0HeKoWv6GOoLdN1cLefoazTY7nTyOCE",
+     authDomain: "zepher-express.firebaseapp.com",
+     databaseURL: "https://zepher-express.firebaseio.com",
+     projectId: "zepher-express",
+     storageBucket: "zepher-express.appspot.com",
+     messagingSenderId: "1044678323580"
+   };
+   firebase.initializeApp(config);
 
-  // console.log(database);
+   let database = firebase.database();
 
-  $("#add-train-btn").on('click', (event) => {
-    event.preventDefault();
+   // console.log(database);
 
-    let name = $("#train-name").val().trim();
-    let destination = $("#train-destination").val().trim();
-    let initialTime = moment($("#initial-time").val().trim(), "HH:mm").unix();
-    let frequency = parseInt($("#train-frequency").val().trim());
+   $("#add-train-btn").on('click', (event) => {
+     event.preventDefault();
 
-    let newTrain = {
-      name: name,
-      destination: destination,
-      initialTime: initialTime,
-      frequency: frequency
-    };
+     let name = $("#train-name").val().trim();
+     let destination = $("#train-destination").val().trim();
+     let initialTime = moment($("#initial-time").val().trim(), "HH:mm").unix();
+     let frequency = parseInt($("#train-frequency").val().trim());
 
-    database.ref().push(newTrain);
+     let newTrain = {
+       name: name,
+       destination: destination,
+       initialTime: initialTime,
+       frequency: frequency
+     };
 
-    $("#train-name").val('');
-    $("#train-destination").val('');
-    $("#initial-time").val('');
-    $("#train-frequency").val('');
+     database.ref().push(newTrain);
 
-    // console.log(initialTime);
-    // return false;
-  });
+     $("#train-name").val('');
+     $("#train-destination").val('');
+     $("#initial-time").val('');
+     $("#train-frequency").val('');
 
-  // listener for adding trains to database and to the table
-  database.ref().on("child_added", (childSnapshot, prevChildkey) => {
-    let name = childSnapshot.val().name;
-    let destination = childSnapshot.val().destination;
-    let initialTime = moment.unix(childSnapshot.val().initialTime);
-    let frequency = childSnapshot.val().frequency;
-    let minutesElapsed = moment().diff(initialTime, 'minutes');
+     // console.log(initialTime);
+     // return false;
+   }, function (errorObject) {
+     console.log("The read failed: " + errorObject.code);
+   });
+
+   // listener for adding trains to database and to the table
+   database.ref().on("child_added", (childSnapshot, prevChildkey) => {
+     let name = childSnapshot.val().name;
+     let destination = childSnapshot.val().destination;
+     let initialTime = moment.unix(childSnapshot.val().initialTime);
+     let frequency = childSnapshot.val().frequency;
+     let minutesElapsed = moment().diff(initialTime, 'minutes');
+
+     let minutesAway = frequency - minutesElapsed % frequency;
+
+     let nextArrival = moment().add(minutesAway, "m").format("HH:mm");
+     console.log(initialTime);
+
+     $("#train-table > tbody").append(`<tr><td> ${name} </td><td> ${destination} </td><td> ${frequency} </td><td> ${nextArrival} </td><td> ${minutesAway} `);
 
 
-    //convert intialtime to unix
+   }, function (errorObject) {
+     console.log("The read failed: " + errorObject.code);
+   });
 
-    let minutesAway = frequency - minutesElapsed % frequency;
+   setInterval(() => {
+    
+   }, 60000);
 
-    let nextArrival = moment().add(minutesAway, "m").format("HH:mm");
-    console.log(initialTime);
-
-    $("#train-table > tbody").append(`<tr><td> ${name} </td><td> ${destination} </td><td> ${frequency} </td><td> ${nextArrival} </td><td> ${minutesAway} `)
-
-  });
+ }());
